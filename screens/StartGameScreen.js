be56e8329b9637f9fec16ca1905a52489be48404
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-15 01:41:19 +0200

    StartGameScreen.js update > button is added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,16 @@
-import { View ,StyleSheet} from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,16 +20,27 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        padding:16,
-        marginTop:100,
-        marginHorizontal:24,
-        borderRadius:8,
-        backgroundColor:'#72063c',
-        elevation:4, //only on android
-        shadowColor:'black',
-        shadowOffset:{width: 0, height:2},
-        shadowRadius:8,
-        shadowOpacity:0.5,
-    }
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    elevation: 4, //only on android
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.5,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
