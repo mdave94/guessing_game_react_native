03427e4d795efd25ed82613c596c1b61425f36bd
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-17 00:03:21 +0200

    LInearGradiant component from expo is added. Design update in StartGameScreen.js

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.screensContainer}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screensContainer : {
+    flex:1,
+  }
+});
