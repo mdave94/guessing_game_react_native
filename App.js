40eb448dde0623091d00c9833a0208e468e82c0e
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-17 17:04:55 +0200

    BackGoundImage is added

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.screensContainer}>
-      <StartGameScreen />
+    <LinearGradient
+      colors={["#4e0329", "#ddb52f"]}
+      style={styles.screensContainer}
+    >
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.screensContainer}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  screensContainer : {
-    flex:1,
+  screensContainer: {
+    flex: 1,
+  },
+  backgroundImage:{
+    opacity: 0.15,
   }
 });
