4d334187d259a06a62c502406082e6231447857b
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-20 10:31:02 +0200

    Title.js component is created

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
+      
       {/* GUESS */}
       <View>
         <Text>Higher or lower</Text>
@@ -20,6 +22,16 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 40,
-    
+
+  },
+  title:{
+    fontSize:24,
+    fontWeight:'bold',
+    color:'#ddb52f',
+    textAlign:'center',
+    borderWidth:2,
+    borderColor:'#ddb52f',
+    padding:12,
+
   },
 });
