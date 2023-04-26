a1656008b96eaefd850baff3f17dadad32113d09
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-26 23:02:55 +0200

    Image added but needs update

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,27 @@
-import {Text} from 'react-native'
+import { Text, View,StyleSheet,Image } from "react-native";
+import Colors from "../constants/colors";
 
 function GamveOverScreen() {
-    return (
-        <Text>GAME OVER </Text>
-    )
+  return (
+    <View>
+      <Text>GAME OVER </Text>
+      <View style={styles.imageContainer}>
+        <Image source={require("../assets/images/success.png")} />
+      </View>
+    </View>
+  );
 }
 
 export default GamveOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 400,
+    height: 400,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.yellow,
+    overflow: "hidden",
+    margin:36,
+  },
+});
