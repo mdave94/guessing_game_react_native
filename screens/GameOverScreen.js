675998c4f5949f37f8e7bc0c64611ec1e4bbdb61
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-27 15:12:02 +0200

    GameOver scrren update

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,24 @@
-import { Text, View,StyleSheet,Image } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import Colors from "../constants/colors";
-
+import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
 function GamveOverScreen() {
   return (
-    <View>
-      <Text>GAME OVER </Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER </Title>
       <View style={styles.imageContainer}>
-        <Image source={require("../assets/images/success.png")} />
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
       </View>
+
+      <Text style={styles.summaryText}>
+        Your phone <Text style={styles.highlight}>X</Text> round to the number{" "}
+        <Text style={styles.highlight}>Y</Text>{" "}
+      </Text>
+
+      <PrimaryButton> New game </PrimaryButton>
     </View>
   );
 }
@@ -15,13 +26,33 @@ function GamveOverScreen() {
 export default GamveOverScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   imageContainer: {
-    width: 400,
-    height: 400,
-    borderRadius: 200,
+    width: 300,
+    height: 300,
+    borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.yellow,
     overflow: "hidden",
-    margin:36,
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom:24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.yellow,
   },
 });
